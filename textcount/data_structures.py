99160d4bb84c362921fbb8ebbf04e7e76f16e1e0
adamggrim <97310482+adamggrim@users.noreply.github.com>
2024-06-03 17:03:29 -0400

    Improve data_structures.py documentation

--- textcount/data_structures.py
+++ textcount/data_structures.py
@@ -13,38 +13,92 @@ class POSCount:
     @property
     def adj_ratio(self) -> float:
         """
-        Calculate the percentage of adjectives relative to the 
-        total word count."""
+        Calculates the ratio of adjectives to total word count.
+
+        Returns:
+            float: The ratio of adjectives to total word count.
+        """
         return self.adj_count / self.word_count * 100 if self.word_count else 0
 
     @property
     def adp_ratio(self) -> float:
+        """
+        Calculates the ratio of adpositions to total word count.
+
+        Returns:
+            float: The ratio of adpositions to total word count.
+        """
         return self.adp_count / self.word_count * 100 if self.word_count else 0
 
     @property
     def adv_ratio(self) -> float:
+        """
+        Calculates the ratio of adverbs to total word count.
+
+        Returns:
+            float: The ratio of adverbs to total word count.
+        """
         return self.adv_count / self.word_count * 100 if self.word_count else 0
 
     @property
     def conj_ratio(self) -> float:
-        return self.conj_count / self.word_count * 100 if self.word_count else 0
+        """
+        Calculates the ratio of conjunctions to total word count.
+        
+        Returns:
+            float: The ratio of conjunctions to total word count.
+        """
+        return (self.conj_count / self.word_count * 100 if self.word_count 
+                else 0)
 
     @property
     def det_ratio(self) -> float:
+        """
+        Calculates the ratio of determiners to total word count.
+        
+        Returns:
+            float: The ratio of determiners to total word count.
+        """
         return self.det_count / self.word_count * 100 if self.word_count else 0
 
     @property
     def noun_ratio(self) -> float:
-        return self.noun_count / self.word_count * 100 if self.word_count else 0
+        """
+        Calculates the ratio of nouns to total word count.
+
+        Returns:
+            float: The ratio of nouns to total word count.
+        """
+        return (self.noun_count / self.word_count * 100 if self.word_count 
+                else 0)
 
     @property
     def prt_ratio(self) -> float:
+        """
+        Calculates the ratio of particles to total word count.
+
+        Returns:
+            float: The ratio of particles to total word count.
+        """
         return self.prt_count / self.word_count * 100 if self.word_count else 0
 
     @property
     def pron_ratio(self) -> float:
-        return self.pron_count / self.word_count * 100 if self.word_count else 0
+        """
+        Calculates the ratio of pronouns to total word count.
+
+        Returns:
+            float: The ratio of pronouns to total word count.
+        """
+        return (self.pron_count / self.word_count * 100 if self.word_count 
+                else 0)
 
     @property
     def verb_ratio(self) -> float:
+        """
+        Calculates the ratio of verbs to total word count.
+
+        Returns:
+            float: The ratio of verbs to total word count.
+        """
         return self.verb_count / self.word_count * 100 if self.word_count else 0
