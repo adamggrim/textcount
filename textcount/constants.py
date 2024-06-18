bf40a0209ba8c8d8eaa9342f5310e0c58151dfd7
adamggrim <97310482+adamggrim@users.noreply.github.com>
2024-06-18 11:52:52 -0400

    Add new constants to constants.py

--- textcount/constants.py
+++ textcount/constants.py
@@ -9,6 +9,7 @@ class HelpMessages:
         MFWS (STR): Help message for --mfws argument
         POS_COUNT (STR): Help message for --pos-count argument
         TIME_TO_READ (STR): Help message for --time-to-read argument
+        WORD_COUNT (STR): Help message for --word-count argument
     """
     DESCRIPTION = ('Specify the text analysis function to apply to the '
                    'clipboard.')
@@ -17,9 +18,16 @@ class HelpMessages:
     MFWS = 'get most frequent words'
     POS_COUNT = 'count parts of speech'
     TIME_TO_READ = 'calculate time to read'
+    WORD_COUNT = 'count words'
 
 # String printed to prompt the user for any other text
 ANY_OTHER_TEXT_STR = 'Any other text? (y/n) (Copy text to clipboard):'
+
+# String printed to prompt the user for the number of most frequent words
+ENTER_MFW_COUNT_STR = 'How many most frequent words?'
+
+# String printed when the user does not enter a number
+ENTER_NUMBER_STR = 'Please enter a number.'
 
 # String printed when the previous response was invalid
 ENTER_VALID_RESPONSE_STR = 'Please enter a valid response.'
@@ -30,8 +38,19 @@ EXIT_STR = 'Exiting the program...'
 # Set of strings for indicating a negative response
 NO_STRS = {'no', 'n'}
 
+# Tuple of strings for POS tags
+POS_TAGS = ('ADJ', 'ADP', 'ADV', 'CONJ', 'DET', 'NOUN', 'NUM', 'PRT', 'PRON', 
+            'VERB', 'X')
+
+# Tuple of strings for POS tags representing words
+POS_KNOWN_TAGS = ('ADJ', 'ADP', 'ADV', 'CONJ', 'DET', 'NOUN', 'PRT', 'PRON', 
+                 'VERB')
+
 # Set of strings for exiting the program
 QUIT_STRS = {'quit', 'q', 'exit', 'e'}
+
+# String printed to prompt the user for words per minute
+WPM_STR = 'How many words per minute?'
 
 # Set of strings for indicating an affirmative response
 YES_STRS = {'yes', 'y'}
